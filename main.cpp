84ca26894f923f844a8003280e4b3c3c9f9351ae
GoombaGoom <106626970+GoombaGoom@users.noreply.github.com>
2022-09-08 08:28:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@ void render(){
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
   };  
-  const int VAO;
-  
-  glDrawArrays();
+  unsigned int VAO;
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glGenVertexArrais(0, &VAO);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 int main(){
   glfwInit();
